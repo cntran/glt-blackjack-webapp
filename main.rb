fd8d4da7bce4291d28de8b5e070329b0d05da9a3
Craig Tran <cntran@gmail.com>
2013-08-22 16:15:57 -0700

    add new files

--- main.rb
+++ main.rb
@@ -1,8 +1,31 @@
 require 'rubygems'
 require 'sinatra'
+require 'pry'
 
 set :sessions, true
 
+get '/' do 
 
+  @player_name = ''
 
+  if session[:player_name]
+    @player_name = session[:player_name]
+  end
 
+  erb :index  
+
+end
+
+post '/setPlayerName' do
+
+  player_name = params[:player_name]
+  session[:player_name] = player_name
+
+  redirect '/'
+
+end
+
+get '/game' do
+  'Game started! ' + session[:player_name]
+end
+ 
