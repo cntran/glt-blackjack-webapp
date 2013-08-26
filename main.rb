3438e3e5755a60823f95b3edcc5481ed69000fcc
Craig Tran <cntran@gmail.com>
2013-08-26 15:45:34 -0700

    add new files

--- main.rb
+++ main.rb
@@ -26,7 +26,6 @@ helpers do
     total
   end
 
-
   def display_card_image(card)
 
     image_src = ''
@@ -64,37 +63,67 @@ end
 get '/' do 
 
   session.delete(:player_name)
-  session.delete(:blackjack)
+  session.delete(:player_balance)
+  session.delete(:deck)
+  session.delete(:player_cards)
+  session.delete(:dealer_cards)
 
   erb :index  
+
+end
+
+get '/rules' do
+
+  erb :rules
+
+end
+
+get '/about' do
+
+  erb :about
 
 end
 
 post '/setPlayerName' do
 
   player_name = params[:player_name]
+
+  if player_name == ''
+    @error = 'You didn\'t enter your name.'
+    halt( erb :index )
+  end
+
   session[:player_name] = player_name
+  session[:player_balance] = 100
 
   redirect '/welcome'
 
 end
 
 get '/welcome' do
-  @player_name = ''
-
-  if session[:player_name]
-    @player_name = session[:player_name]
-  end
 
   erb :welcome
 end
 
 
-get '/game' do
+post '/game' do
+
+  # validate bet amount
+  bet = params['bet']
+  if bet.to_i == 0
+    @error = 'Please enter in a numeric value.'
+    halt( erb :welcome)
+  elsif bet.to_i > session[:player_balance].to_i
+    @error = 'You don\'t have enough cash to bet that amount.'
+    halt( erb :welcome)
+  end
+
+  session[:bet] = bet
 
   suits = ['C', 'D', 'H', 'S']
   values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
   session[:deck] = suits.product(values).shuffle!
+
 
   # deal cards
   session[:dealer_cards] = []
@@ -104,16 +133,25 @@ get '/game' do
   session[:dealer_cards] << session[:deck].pop
   session[:player_cards] << session[:deck].pop
 
+  @show_player_actions = true
+  @dealer_turn = false
   erb :game
 
 end
 
 post '/game/hit' do
 
+  @show_player_actions = true
+  @game_over = false
+  @dealer_turn = false
+
   session[:player_cards] << session[:deck].pop
   hand_value = calculate_total(session[:player_cards])
   if hand_value > 21
-    @error = 'Sorry you busted! ' 
+    @error = 'Looks like you busted. :(' 
+    @show_player_actions = false
+    @game_over = true
+    session[:player_balance] = session[:player_balance].to_i - session[:bet].to_i
   end
 
   erb :game
@@ -122,7 +160,62 @@ end
 
 post '/game/stay' do
 
+  @dealer_turn = true
+  @show_player_actions = false
+  @game_over = false
 
+  player_hand_value = calculate_total(session[:player_cards])
+  dealer_hand_value = calculate_total(session[:dealer_cards])
+
+
+  if dealer_hand_value > 16 # end game and determine winner
+
+    if player_hand_value > dealer_hand_value  
+      @success = 'You win! :)'
+      session[:player_balance] = session[:player_balance].to_i + session[:bet].to_i
+    elsif player_hand_value < dealer_hand_value
+      @error = 'Sorry, you lose. :('
+      session[:player_balance] = session[:player_balance].to_i - session[:bet].to_i
+    elsif player_hand_value == dealer_hand_value
+      @success = 'Its a push. :/'
+    end
+
+    @game_over = true
+
+  end
+
+  erb :game
+
+end
+
+post '/game/hit/dealer' do
+
+  @show_player_actions = false
+  @game_over = false
+  @dealer_turn = true
+
+  session[:dealer_cards] << session[:deck].pop
+  player_hand_value = calculate_total(session[:player_cards])
+  dealer_hand_value = calculate_total(session[:dealer_cards])
+
+  if dealer_hand_value > 21
+    @success = 'Dealer busts! You win! :)' 
+    @game_over = true
+  elsif dealer_hand_value > 16
+
+    if player_hand_value > dealer_hand_value  
+      @success = 'You win! :)'
+      session[:player_balance] = session[:player_balance].to_i + session[:bet].to_i
+    elsif player_hand_value < dealer_hand_value
+      @error = 'Sorry, you lose. :('
+      session[:player_balance] = session[:player_balance].to_i - session[:bet].to_i
+    elsif player_hand_value == dealer_hand_value
+      @success = 'Its a push. :/'
+    end
+
+    @game_over = true
+
+  end
 
   erb :game
 
